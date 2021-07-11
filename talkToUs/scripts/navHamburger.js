9c1bc3b39c5081e9970e190ac8a5d09eae378a31
dexeloper <76111113+dexeloper@users.noreply.github.com>
2021-07-11 10:35:05 +0530

    FCommit 3

--- talkToUs/scripts/navHamburger.js
+++ talkToUs/scripts/navHamburger.js
@@ -3,3 +3,7 @@ $(".navHamDropContainer").hide();
 $("#navHamBtn").click(function() {
   $(".navHamDropContainer").fadeToggle();
 });
+
+setTimeout(function(){
+  $("#opacity-load").fadeOut();
+}, 500);
